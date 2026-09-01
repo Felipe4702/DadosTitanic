08fe738ab5b8a6c4d701decfce5c038175c686d9
Cassiuscray Felipe <cassiuscaryfelipe@gmail.com>
2026-09-01 19:10:04 -0300

    feito a funcao de separar as idades por genero e classe, com um grafico de caixas

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ import seaborn as sns
 
 df = pd.read_csv('data/titanic.csv', sep=',')
 
+def idade_genero_classe(df):
+    #agrupa as estatisticas por sexo e clase, dentro disso pega e idade e faz as operacoes matematicas
+    estatisticas = df.groupby (['Sex', 'Pclass'])['Age'].agg(['mean', 'median', 'std']).round(2)
+    print("e\Estatisticas Idade por Genero e Classe:")
+    print(estatisticas)
+
+
+    #faz um grafico de caixas com o eixo x sendo as 3 classes e o y a idade, com sexos sendo diferenciados por cores
+    plt.figure(figsize = (10,6))
+    sns.boxplot(data=df, x = 'Pclass', y = 'Age', hue = 'Sex', palette = 'Set2')
+    plt.title('Estatisticas Idade por Genero e Classe')
+    plt.xlabel('Classe Social')
+    plt.ylabel('Idade')
+    plt.legend(title = 'Genero')
+
+    #as caixas marcam onde comeca o 2 quartil e onde comeca o 4, com a linha horizontal sendo a mediana
+    #a altura das linhas verticais sao calculadas como sendo 1.5 vezes o tamanho da caixa
+    plt.show()
+
+    return estatisticas
+
+estatisticas = idade_genero_classe(df)
